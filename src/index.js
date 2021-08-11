96386486ab93926048ca50d75f786d3b82daccdc
Alex Iudin <A.S.Yudinmail@gmail.com>
2021-08-11 17:33:19 +0300

    init: start calc

--- src/index.js
+++ src/index.js
@@ -3,8 +3,28 @@ function eval() {
     return;
 }
 
+const reg = /\(([^)]+)\)/g;
+
+function newSub(item) {
+    let arr = item.split('');
+    switch (arr[1]) {
+        case '+':
+            return Number(arr[0]) + Number(arr[2])
+        case '-': 
+            return Number(arr[0]) - Number(arr[2])
+        case '*':
+            return Number(arr[0]) * Number(arr[2])
+        case '/': 
+        if(arr[2] == 0) throw 'Division by zero'
+         return Number(arr[0]) / Number(arr[2]);
+        default:
+            break;
+    }
+}
+
 function expressionCalculator(expr) {
-    // write your solution here
+    let result = expr.replace(/\s/g, '');
+    return +result.replace(/\d.\d/g, newSub)
 }
 
 module.exports = {
