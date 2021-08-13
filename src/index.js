ade5db413a8c7204c4fa9f3da61e4bd03d68b85b
Alex Iudin <A.S.Yudinmail@gmail.com>
2021-08-13 17:00:51 +0300

    fix: change function and regular

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
-const REG_PLUS = /((\d+\.\d+)|\d+)(\+)((\d+\.\d+)|\d+)/;
+const REG_ADDITION = /((\d+\.\d+)|\d+)(\+)((\d+\.\d+)|\d+)/;
 const REG_MINUS = /((\d+\.\d+)|\d+)(\-)((\d+\.\d+)|\d+)/;
-const REG_MODMINUS = /\-((\d+\.\d+)|\d+)(\-)((\d+\.\d+)|\d+)/;
-const REG_MODMINUSNEW = /^\-((\d+\.\d+)|\d+)(\-)((\d+\.\d+)|\d+)$/;
-const REG_MODPLUSNEW = /^\-((\d+\.\d+)|\d+)(\+)((\d+\.\d+)|\d+)$/;
-const REG_MODPLUS = /\-((\d+\.\d+)|\d+)(\+)((\d+\.\d+)|\d+)/;
 const REG_MULTIPLY = /((\d+\.\d+)|\d+)(\*)((\d+\.\d+)|\d+)/;
-const REG_DIVIDE = /((\d+\.\d+)|\d+)(\/)((\d+\.\d+)|\d+)/;
-const REG_CALC = /((\d+\.\d+)|\d+)(\*|\/|\-|\+)((\d+\.\d+)|\d+)/;
+const REG_DIVISION = /((\d+\.\d+)|\d+)(\/)((\d+\.\d+)|\d+)/;
+const REG_CALC = /((\d+\.\d+)|\d+)(\/|\-|\+|\*)((\d+\.\d+)|\d+)/;
+const REG_MOD = /((\d+\.\d+)|\d+)(\+\-|\*\-|\/\-)((\d+\.\d+)|\d+)/;
+const REG_MINUSNUMBMINUS = /((\-\d+\.\d+)|\-\d+)(\-|\+)((\d+\.\d+)|\d+)/;
 const REG_BRACKETS = /((\(\d+\.\d+\))|(\(\-\d+\.\d+\))|\(\d+\)|\(\-\d+\))/;
-const REG_MOD = /((\d+\.\d+)|\d+)(\+\-|\-\*)((\d+\.\d+)|\d+)/;
 
 let result = 0;
 
@@ -35,59 +32,83 @@ function newSub(item) {
     }
 }
 
-function mod(item) {
-    let arr = item.split(/(\+\-|\-\*)/);
+function calcOnMinusNumb(item) {
+    let arr = item.split(/(\+\-|\*\-|\/\-)/);
     switch (arr[1]) {
         case '+-':
             return Number(arr[0]) - Number(arr[2])
-        case '-*':
+        case '*-':
             return -Number(arr[0]) * Number(arr[2])
+        case '/-':
+            return -Number(arr[0]) / Number(arr[2])
     }
-    
 }
 
-function modPlus(item) {
-    let arr = item.split(/(\+)/);
-    return  Math.abs(Number(arr[2])) - Math.abs(Number(arr[0]))
-}
-
-function modMinus(item) {
-    let arr = item.split(/(\-\d+\.\d+|\-\d+|\+\d+\.\d+|\+\d+|)/);
-    return  Number(arr[1]) + Number(arr[3]) // баг в регулярке
+function calcMinusOnMinusNumb(item) {
+    let arr = item.split('+');
+     if (Number(arr[0]) + Number(arr[1])) {
+         return Number(arr[0]) + Number(arr[1])}
+    else {
+        arr = item.split('-');
+        return -(Number(arr[1]) + Number(arr[2])) // bug split
+    }
 }
 
 function excOper(item) {
     result = item
-    if(result.match(REG_DIVIDE)) {
-        result = result.replace(REG_DIVIDE, newSub)
-        if(result.match(REG_DIVIDE)) excOper(result)
+    if (result.match(REG_BRACKETS)) {
+        result = result.replace(/\(|\)/g, '');
+    }
+    if(result.match(REG_DIVISION)) {
+        result = result.replace(REG_DIVISION, newSub)
+        if(result.match(REG_DIVISION)) excOper(result)
     }
     if(result.match(REG_MULTIPLY)) {
         result = result.replace(REG_MULTIPLY, newSub)
         if(result.match(REG_MULTIPLY)) excOper(result)
     }
-    if(result.match(REG_MODMINUSNEW)) {
-        result = result.replace(REG_MODMINUS, modMinus)
-    }
-    if(result.match(REG_MODPLUSNEW)) {
-        result = result.replace(REG_MODPLUSNEW, modMinus)
-    }
     if(result.match(REG_MINUS)) {
         result = result.replace(REG_MINUS, newSub)
-        if (result.match(REG_MODPLUS)) {
-            result = result.replace(REG_MODPLUS, modPlus)
+        if (result.match(REG_MOD)) {
+            result = result.replace(REG_MOD, calcOnMinusNumb)
+            if(result.match(REG_MOD)) excOper(result)
+        }
+        if (result.match(REG_MINUSNUMBMINUS)) {
+            result = result.replace(REG_MINUSNUMBMINUS, calcMinusOnMinusNumb)
+            excOper(result)
         }
         if(result.match(REG_MINUS)) excOper(result)
+        if (result.match(REG_BRACKETS)) {
+            result = result.replace(/\(|\)/g, '');
+            if (result.match(REG_MOD)) {
+                result = result.replace(REG_MOD, calcOnMinusNumb)
+                if(result.match(REG_MOD)) excOper(result)
+            }
+            if (result.match(REG_MINUSNUMBMINUS)) {
+                result = result.replace(REG_MINUSNUMBMINUS, calcMinusOnMinusNumb)
+                excOper(result)
+            }
+            excOper(result);
+        }
     }
-    if (result.match(REG_MOD)) {
-        result = result.replace(REG_MOD, mod)
+    if (result.match(REG_MINUSNUMBMINUS)) {
+        result = result.replace(REG_MINUSNUMBMINUS, calcMinusOnMinusNumb)
+        excOper(result)
     }
-    if(result.match(REG_PLUS)) {
-        result = result.replace(REG_PLUS, newSub)
-        if(result.match(REG_PLUS)) excOper(result)
+    if(result.match(REG_ADDITION)) {
+        result = result.replace(REG_ADDITION, newSub)
+        if(result.match(REG_ADDITION)) excOper(result)
     }
     if (result.match(REG_BRACKETS)) {
-        result = result.replace(/\(|\)/, '')
+        result = result.replace(/\(|\)/g, '');
+        if (result.match(REG_MOD)) {
+            result = result.replace(REG_MOD, calcOnMinusNumb)
+            if(result.match(REG_MOD)) excOper(result)
+        }
+        if (result.match(REG_MINUSNUMBMINUS)) {
+            result = result.replace(REG_MINUSNUMBMINUS, calcMinusOnMinusNumb)
+            excOper(result)
+        }
         excOper(result);
     }
     if (result.match(REG_CALC)) excOper(result)
@@ -103,11 +124,11 @@ function validParentheses(parens){
       if (n < 0) return false;
     }
     return n == 0;
-  }
+}
 
 function expressionCalculator(expr) {
     let exp = expr.replace(/\s/g, '');
-    if (!validParentheses(exp)) throw "ExpressionError: Brackets must be paired"; 
+    // if (!validParentheses(expr)) throw "ExpressionError: Brackets must be paired"; 
     return excOper(exp);
 }
 
