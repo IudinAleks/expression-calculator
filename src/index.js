76a97a96161a4c2d585768ba16f1369f8eb2b913
Alex Iudin <A.S.Yudinmail@gmail.com>
2021-08-15 23:00:33 +0300

    fix: change work of minus arguments

--- src/index.js
+++ src/index.js
@@ -1,21 +1,23 @@
 const REG_ADDITION = /((\d+\.\d+)|\d+)(\+)((\d+\.\d+)|\d+)/;
+const REG_MINUSNUMBADDITION = /((\-\d+\.\d+)|\-\d+)(\+)((\d+\.\d+)|\d+)/;
 const REG_MINUS = /((\d+\.\d+)|\d+)(\-)((\d+\.\d+)|\d+)/;
-const REG_MULTIPLY = /((\d+\.\d+)|\d+)(\*)((\d+\.\d+)|\d+)/;
-const REG_DIVISION = /((\d+\.\d+)|\d+)(\/)((\d+\.\d+)|\d+)/;
+const REG_MULTIPLY = /((\-\d+\.\d+)|\-\d+|(\d+\.\d+)|\d+)(\*)((\-\d+\.\d+)|\-\d+|(\d+\.\d+)|\d+)/;
+const REG_MINUSNUMBMULTIPLY = /((\-\d+\.\d+)|\-\d+|(\d+\.\d+)|\d+)(\*)((\-\d+\.\d+)|\-\d+|(\d+\.\d+)|\d+)/;
+const REG_DIVISION = /((\-\d+\.\d+)|\-\d+|(\d+\.\d+)|\d+)(\/)((\-\d+\.\d+)|\-\d+|(\d+\.\d+)|\d+)/;
+const REG_MINUSNUMBDIVISION = /((\-\d+\.\d+)|\-\d+|(\d+\.\d+)|\d+)(\/)((\-\d+\.\d+)|\-\d+|(\d+\.\d+)|\d+)/;
 const REG_CALC = /((\d+\.\d+)|\d+)(\/|\-|\+|\*)((\d+\.\d+)|\d+)/;
-const REG_MOD = /((\d+\.\d+)|\d+)(\+\-|\*\-|\/\-)((\d+\.\d+)|\d+)/;
-const REG_MINUSNUMBMINUS = /((\-\d+\.\d+)|\-\d+)(\-|\+)((\d+\.\d+)|\d+)/;
+const REG_MOD = /((\d+\.\d+)|\d+|(\-\d+\.\d+)|\-\d+)(\+\-|\*\-|\/\-)((\d+\.\d+)|\d+)/;
+const REG_MINUSNUMBPLUS = /((\-\d+\.\d+)|\-\d+)(\+)((\d+\.\d+)|\d+)/;
+const REG_MINUSNUMBMINUS = /((\-\d+\.\d+)|\-\d+)(\-)((\d+\.\d+)|\d+)/;
 const REG_BRACKETS = /((\(\d+\.\d+\))|(\(\-\d+\.\d+\))|\(\d+\)|\(\-\d+\))/;
-
-let result = 0;
+const REG_BRACKETSARR = /\(.+\)/;
 
 function eval() {
     // Do not use eval!!!
     return;
 }
 
-
-function newSub(item) {
+function mainOperations(item) {
     let arr = item.split(/(\-|\*|\+|\/)/);
     switch (arr[1]) {
         case '*':
@@ -40,14 +42,29 @@ function calcOnMinusNumb(item) {
         case '*-':
             return -Number(arr[0]) * Number(arr[2])
         case '/-':
+            if (arr[2] == 0) throw 'TypeError: Division by zero.';
             return -Number(arr[0]) / Number(arr[2])
+    }
+}
+
+function calc(item) {
+    let arr = item.split(/(\*|\/|\+)/);
+    switch (arr[1]) {
+        case '*':
+            return Number(arr[0]) * Number(arr[2])
+        case '/':
+            if (arr[2] == 0) throw 'TypeError: Division by zero.';
+            return Number(arr[0]) / Number(arr[2])
+        case '+':
+            return Number(arr[0]) + Number(arr[2])
     }
 }
 
 function calcMinusOnMinusNumb(item) {
     let arr = item.split('+');
      if (Number(arr[0]) + Number(arr[1])) {
-         return Number(arr[0]) + Number(arr[1])}
+         return Number(arr[0]) + Number(arr[1])
+        }
     else {
         arr = item.split('-');
         return -(Number(arr[1]) + Number(arr[2])) // bug split
@@ -55,80 +72,47 @@ function calcMinusOnMinusNumb(item) {
 }
 
 function excOper(item) {
-    result = item
-    if (result.match(REG_BRACKETS)) {
-        result = result.replace(/\(|\)/g, '');
+    while(item.match(REG_DIVISION)) {
+        item=item.replace(REG_MINUSNUMBDIVISION, calc);
     }
-    if(result.match(REG_DIVISION)) {
-        result = result.replace(REG_DIVISION, newSub)
-        if(result.match(REG_DIVISION)) excOper(result)
+    while(item.match(REG_MULTIPLY)) {
+        item=item.replace(REG_MINUSNUMBMULTIPLY, calc);
     }
-    if(result.match(REG_MULTIPLY)) {
-        result = result.replace(REG_MULTIPLY, newSub)
-        if(result.match(REG_MULTIPLY)) excOper(result)
+    while(item.match(REG_MINUS)) {
+        item= item.replace(REG_MINUSNUMBMINUS, calcMinusOnMinusNumb);
+        item=item.replace(REG_MINUS, mainOperations);
+        item=item.replace(REG_MOD, calcOnMinusNumb);
+        item= item.replace(REG_MINUSNUMBPLUS, calcMinusOnMinusNumb)
     }
-    if(result.match(REG_MINUS)) {
-        result = result.replace(REG_MINUS, newSub)
-        if (result.match(REG_MOD)) {
-            result = result.replace(REG_MOD, calcOnMinusNumb)
-            if(result.match(REG_MOD)) excOper(result)
-        }
-        if (result.match(REG_MINUSNUMBMINUS)) {
-            result = result.replace(REG_MINUSNUMBMINUS, calcMinusOnMinusNumb)
-            excOper(result)
-        }
-        if(result.match(REG_MINUS)) excOper(result)
-        if (result.match(REG_BRACKETS)) {
-            result = result.replace(/\(|\)/g, '');
-            if (result.match(REG_MOD)) {
-                result = result.replace(REG_MOD, calcOnMinusNumb)
-                if(result.match(REG_MOD)) excOper(result)
-            }
-            if (result.match(REG_MINUSNUMBMINUS)) {
-                result = result.replace(REG_MINUSNUMBMINUS, calcMinusOnMinusNumb)
-                excOper(result)
-            }
-            excOper(result);
-        }
+    while(item.match(REG_ADDITION)) {
+        item= item.replace(REG_MINUSNUMBADDITION, calc);
+        item=item.replace(REG_ADDITION, mainOperations);
+        item=item.replace(REG_MOD, calcOnMinusNumb);
     }
-    if (result.match(REG_MINUSNUMBMINUS)) {
-        result = result.replace(REG_MINUSNUMBMINUS, calcMinusOnMinusNumb)
-        excOper(result)
-    }
-    if(result.match(REG_ADDITION)) {
-        result = result.replace(REG_ADDITION, newSub)
-        if(result.match(REG_ADDITION)) excOper(result)
-    }
-    if (result.match(REG_BRACKETS)) {
-        result = result.replace(/\(|\)/g, '');
-        if (result.match(REG_MOD)) {
-            result = result.replace(REG_MOD, calcOnMinusNumb)
-            if(result.match(REG_MOD)) excOper(result)
-        }
-        if (result.match(REG_MINUSNUMBMINUS)) {
-            result = result.replace(REG_MINUSNUMBMINUS, calcMinusOnMinusNumb)
-            excOper(result)
-        }
-        excOper(result);
-    }
-    if (result.match(REG_CALC)) excOper(result)
-    
-    return +result;
+    return +item.replace(/\(|\)/g, '');
 }
-
 function validParentheses(parens){
     let n = 0;
     for (let i = 0; i < parens.length; i++) {
-      if (parens[i] == '(') n++;
-      if (parens[i] == ')') n--;
-      if (n < 0) return false;
+        if (parens[i] == '(') n++;
+        if (parens[i] == ')') n--;
+        if (n < 0) return false;
     }
     return n == 0;
 }
 
 function expressionCalculator(expr) {
     let exp = expr.replace(/\s/g, '');
-    // if (!validParentheses(expr)) throw "ExpressionError: Brackets must be paired"; 
+    //# валидация скобок
+    // if (!validParentheses(exp)) throw "ExpressionError: Brackets must be paired"; 
+    
+    //#проверим есть ли что в скобках
+    if (exp.match(REG_BRACKETSARR)) {
+        exp=exp.replace(REG_BRACKETSARR, ()=> {
+            let arr =  exp.match(REG_BRACKETSARR);
+            return excOper(arr[0])
+        })
+    }
     return excOper(exp);
 }
 
